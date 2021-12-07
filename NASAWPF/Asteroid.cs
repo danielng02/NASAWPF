efc60bb0e8e616977446de79dfe7fb6aea34f39e
Dan nguyen <danek.nguyen@gmail.com>
2021-12-07 14:17:09 +0100

    Přidejte soubory projektu.

---
+++ NASAWPF/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NASAWPF
+{
+    public class Asteroid
+    {
+        private DateTime date;
+        private string name;
+        private string id;
+        private string neoID;
+        private string jplURL;
+        private double absMagnitude;
+        private double estDiameter;
+        private bool isHazardous;
+        private bool isSentry;
+
+        public DateTime Date { get => date; set => date = value; }
+        public string Name { get => name; set => name = value; }
+        public string Id { get => id; set => id = value; }
+        public string NeoID { get => neoID; set => neoID = value; }
+        public string JplURL { get => jplURL; set => jplURL = value; }
+        public double AbsMagnitude { get => absMagnitude; set => absMagnitude = value; }
+        public double EstDiameter { get => estDiameter; set => estDiameter = value; }
+        public bool IsHazardous { get => isHazardous; set => isHazardous = value; }
+        public bool IsSentry { get => isSentry; set => isSentry = value; }
+    }
+}
---
+++ NASAWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NASAWPF
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            string startDate = Convert.ToDateTime(DatePickStart.Text).ToString("yyyy-MM-dd");
+            string endDate = Convert.ToDateTime(DatePickEnd.Text).ToString("yyyy-MM-dd");
+            string JSON = String.Format("https://api.nasa.gov/neo/rest/v1/feed?start_date={0}&end_date={1}&api_key=fDYUz8mf87gs1b8txmdHol2s7eTlQLJeL9PfnGql", startDate, endDate);
+            label01.Content = JSON;
+        }
+    }
+}
